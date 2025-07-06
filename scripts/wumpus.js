9291877b5562d78be0dc739115cd46749c386cbd
Mohammad Ismail Hossain <hmismail360@gmail.com>
2025-07-06 20:40:27 +0600

    wumpus finalizewd

--- scripts/wumpus.js
+++ scripts/wumpus.js
@@ -15,8 +15,7 @@ class Wumpus {
 
     display() {
         if (this.alive) {
-            var gap = this.world.roomSize/10;
-            image(wumpus_image, this.position.x * this.world.roomSize + gap, this.position.y * this.world.roomSize + gap, this.world.roomSize - 2*gap, this.world.roomSize - 2*gap);
+            image(wumpus_image, this.position.x * this.world.roomSize, this.position.y * this.world.roomSize, this.world.roomSize, this.world.roomSize);
         } else {
             image(wumpus_dead_image, this.position.x * this.world.roomSize, this.position.y * this.world.roomSize, this.world.roomSize, this.world.roomSize);
         }
