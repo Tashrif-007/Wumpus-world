3871d479aa99f9cfc1a7c526a1faef1788a1d408
Mohammad Ismail Hossain <hmismail360@gmail.com>
2025-07-06 19:43:10 +0600

    gold finalizewd

--- scripts/gold.js
+++ scripts/gold.js
@@ -5,7 +5,7 @@ class Gold {
     }
 
     display() {
-        var gap = this.world.roomSize / 10;
-        image(gold_image, this.position.x * this.world.roomSize + gap, this.position.y * this.world.roomSize + gap, this.world.roomSize - 2*gap, this.world.roomSize -2*gap);
+        let gap = this.world.roomSize / 10;
+        image(gold_image, this.position.x * this.world.roomSize + gap, this.position.y * this.world.roomSize + gap, this.world.roomSize - 2 * gap, this.world.roomSize - 2 * gap);
     }
 }
